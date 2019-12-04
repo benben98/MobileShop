5c09bd65237978f316b772f793e7bd587df65c9e
benben98 <849874507@qq.com>
2019-12-04 08:10:50 +0800

    Initial commit

---
+++ app/src/main/java/com/huatec/dongxinyuan/common/Constants.java
@@ -0,0 +1,7 @@
+package com.huatec.dongxinyuan.common;
+
+public class Constants {
+    public static final String BASE_URL = "http://10.216.18.202:8080/MobileShop/";
+
+    public static final String ACTION_LOGIN="com.edu.huatec.mobileshop.LOGIN";
+}
---
+++ app/src/main/java/com/huatec/dongxinyuan/fragment/CartFragment.java
@@ -0,0 +1,12 @@
+package com.huatec.dongxinyuan.fragment;
+
+import com.huatec.dongxinyuan.R;
+import com.huatec.dongxinyuan.common.BaseFragment;
+
+
+
+public class CartFragment extends BaseFragment {
+    public int getContentViewId(){
+        return R.layout.fragment_cart;
+    }
+}
